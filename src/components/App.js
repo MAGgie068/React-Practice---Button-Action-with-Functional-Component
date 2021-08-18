5ae9b572e66431254cf2ad2b65b16c1040fa87ed
MAGgie <mganant08@gmail.com>
2021-08-18 19:28:50 +0530

    fixed

--- src/components/App.js
+++ src/components/App.js
@@ -2,16 +2,18 @@ import React, {Component, useState} from "react";
 import "./../styles/App.css";
 
 function App() {
+  const [hide, setHide] = useState(true);
   return (
     <div id="main">
-      <button id="click" onClick={show}>Show Me</button>
-      <p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
+      <button id="click" onClick={() => {
+        setHide(!hide);
+      }}
+      >Show Me</button>
+      {hide ? null : 
+      (<p id="para">Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy</p>
+      )}
     </div>
   );
-}
-
-function show(){
-  document.getElementById('para').style.visibility = "hidden";
 }
 
 
